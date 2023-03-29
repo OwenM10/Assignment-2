e6304b63de1ede526fd961ad67cc9046075740e4
OwenM10 <124073767+OwenM10@users.noreply.github.com>
2023-03-29 23:32:47 +0000

    added some stuff

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const ejs = require("ejs");
 const path = require("path");
 const app = express();
 
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 const PORT = 3000;
@@ -11,17 +13,25 @@ app.listen(PORT, () => {
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/views/", "index.html"));
+  //res.sendFile(path.resolve(__dirname + "/views/", "index.html"));
+  res.render("index");
 });
 
 app.get("/profile", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/views/", "profile.html"));
+  //res.sendFile(path.resolve(__dirname + "/views/", "profile.html"));
+  res.render("profile");
 });
 
 app.get("/math", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/views/", "math.html"));
+  //res.sendFile(path.resolve(__dirname + "/views/", "math.html"));
+  res.render("math");
 });
 
 app.get("/profile2 ", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/views/", "profile.html"));
+  //res.sendFile(path.resolve(__dirname + "/views/", "profile.html"));
+  res.render("profile2");
+});
+
+app.get("/slides", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/views/", "slide-show.html"));
 });
