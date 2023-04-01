a9b19822bcf1efd7a583e393a0ef809447e819f6
OwenM10 <124073767+OwenM10@users.noreply.github.com>
2023-04-01 20:51:16 +0000

    Fixed menu bar

--- public/js/profile.js
+++ public/js/profile.js
@@ -27,16 +27,10 @@ const resetErrors = () => {
 const onSubmit = (evt) => {
   resetErrors();
   evt.preventDefault();
-  //TODO:: Use this boolean to keep track of any errors because you need to prevent the settings
-  //       from updating if even one field is wrong
+  
   let formErrors = false;
 
-  //TODO:: Make sure name fields are not empty
-  //TODO:: Validate email with the Regular Expression
-  //TODO:: Validate password with the Regular Expression
-  //TODO:: Validate passwords to be matching
-  //TODO:: Make sure date of birth is in the past.
-  //TODO:: check you formErrors boolean and update the fields if still false
+ 
 
   let firstName = $("#first_name").value;
   let lastName = $("#last_name").value;
